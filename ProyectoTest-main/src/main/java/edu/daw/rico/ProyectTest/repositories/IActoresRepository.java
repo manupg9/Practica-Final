caf5ef20000b4fffe7f3a20a75371091629a667c
manupg9 <manuelpg2004@gmail.com>
2025-11-17 13:54:05 +0100

    Delete ProyectoTest-main directory

--- ProyectoTest-main/src/main/java/edu/daw/rico/ProyectTest/repositories/IActoresRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.daw.rico.ProyectTest.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.daw.rico.ProyectTest.model.Actor;
-
-public interface IActoresRepository extends JpaRepository<Actor, Long> {
-    
-}
--- ProyectoTest-main/src/main/java/edu/daw/rico/ProyectTest/services/ICriticasService.java
+++
@@ -1,9 +0,0 @@
-package edu.daw.rico.ProyectTest.services;
-
-import java.util.List;
-
-import edu.daw.rico.ProyectTest.model.Critica;
-
-public interface ICriticasService {
-    public List<Critica> listarCriticas();
-}
